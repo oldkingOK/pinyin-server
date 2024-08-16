106c998ac14795b5470014453c8b746109dc7252
oldkingOK <oldkingok@qq.com>
2024-08-17 01:27:20 +0800

    添加跨域请求许可

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const CompleteDict = require('@pinyin-pro/data/complete');
 const { segment, OutputFormat, addDict } = require('pinyin-pro');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.post('/', (req, res) => {
     console.log(req.body);
